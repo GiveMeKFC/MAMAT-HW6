71e444f6a6e9a9662a3c09b929ffede36a868ebb
yuval <yuvalsan4545@gmail.com>
2026-07-10 13:14:22 +0300

    more files

--- L4.cpp
+++ L4.cpp
@@ -4,5 +4,24 @@
 class l4_packet : public generic_packet {
     public:
 
+    bool validate_packet(open_port_vec open_ports,
+                                uint8_t ip[IP_V4_SIZE],
+                                uint8_t mask,
+                                uint8_t mac[MAC_SIZE]) {
+
+
+
+    }
+    bool proccess_packet(open_port_vec &open_ports,
+                                uint8_t ip[IP_V4_SIZE],
+                                uint8_t mask,
+                                memory_dest &dst) {
+
+    }
+
+    bool as_string(std::string &packet) {
+
+    }
+
 
 };
